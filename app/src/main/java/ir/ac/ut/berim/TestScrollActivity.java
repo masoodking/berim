85e49962981247f3a1b9acaf8eb12ed0f6546c0e
masoud <eng.masood88@gmail.com>
2015-11-27 02:05:33 +0330

    remove v21 resource

--- app/src/main/java/ir/ac/ut/berim/TestScrollActivity.java
+++ app/src/main/java/ir/ac/ut/berim/TestScrollActivity.java
@@ -1,36 +1,30 @@
 package ir.ac.ut.berim;
 
-import android.graphics.Color;
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.github.ksoichiro.android.observablescrollview.ObservableListView;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-import com.nineoldandroids.view.ViewHelper;
-
 import java.util.ArrayList;
 
-import ir.ut.ac.widget.ViewPager;
-
-public class TestScrollActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+public class TestScrollActivity extends AppCompatActivity {
 
     private View mToolbarView;
+
     private View mBackgroundImage;
+
     private ObservableListView listView;
+
     TextView mtextView;
+
     String temp;
+
     private View mStickyHeader;
 
     @Override
@@ -40,7 +34,6 @@ public class TestScrollActivity extends AppCompatActivity implements ObservableS
 
         listView = (ObservableListView) findViewById(R.id.list);
         mStickyHeader = findViewById(R.id.placeHeaderMenuSticky);
-        listView.setScrollViewCallbacks(this);
         ArrayList<String> items = new ArrayList<String>();
         for (int i = 1; i <= 100; i++) {
             items.add("Item " + i);
@@ -51,24 +44,23 @@ public class TestScrollActivity extends AppCompatActivity implements ObservableS
         ViewGroup header = (ViewGroup) inflater.inflate(R.layout.place_header, listView,
                 false);
         listView.addHeaderView(header, null, false);
-    }
 
-    @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-        if(listView.getChildAt(1).getTop()<85)
-            mStickyHeader.setVisibility(View.VISIBLE);
-        else
-            mStickyHeader.setVisibility(View.GONE);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (listView != null && listView.getChildAt(1) != null
+                        && listView.getChildAt(1).getTop() < 85) {
+                    mStickyHeader.setVisibility(View.VISIBLE);
+                } else {
+                    mStickyHeader.setVisibility(View.GONE);
+                }
+            }
 
-    }
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                    int totalItemCount) {
 
-    @Override
-    public void onDownMotionEvent() {
-
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-
+            }
+        });
     }
 }
