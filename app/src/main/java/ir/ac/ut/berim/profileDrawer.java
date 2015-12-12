cf05a5dd630392fc89d47c88ba884fd079c385d2
saeedalvand <saeedalvandkoohi@yahoo.com>
2015-12-12 22:56:00 +0330

    JUST LET ME DO THE FUCKING PULL

---
+++ app/src/main/java/ir/ac/ut/berim/profileDrawer.java
@@ -0,0 +1,17 @@
+package ir.ac.ut.berim;
+
+import android.os.Bundle;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+
+/**
+ * Created by saeed on 12/12/2015.
+ */
+public class profileDrawer extends MaterialNavigationDrawer {
+
+        @Override
+        public void init(Bundle savedInstanceState) {
+
+        }
+    }
+
