7db155ce60be780fa02502b2d8f5d17c913b4639
saeedalvand <saeedalvandkoohi@yahoo.com>
2016-01-16 01:06:24 +0330

    triple des

---
+++ app/src/main/java/ir/ac/ut/widget/TripleDES.java
@@ -0,0 +1,105 @@
+package ir.ac.ut.widget;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class TripleDES {
+
+	public static byte[] encrypt(byte[] message, byte[] keyBytes) {
+		final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+		final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+		byte[] cipherText = new byte[8];
+		try {
+			final Cipher cipher = Cipher.getInstance("DESede/CBC/NoPadding");
+			cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+			cipherText = cipher.doFinal(message);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
+	
+	public static byte[] encryptECB(byte[] message, byte[] keyBytes) {
+		final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+		byte[] cipherText = new byte[8];
+		try {
+			final Cipher cipher = Cipher.getInstance("DESede/ECB/NoPadding");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+
+			cipherText = cipher.doFinal(message);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
+	
+	public static byte[] decrypt(byte[] message, byte[] keyBytes){
+		final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+		final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+		byte[] plainText = new byte[8];
+		try {
+			Cipher decipher = Cipher.getInstance("DESede/CBC/NoPadding");
+			decipher.init(Cipher.DECRYPT_MODE, key, iv);
+
+			plainText = decipher.doFinal(message);
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return plainText;
+	}
+}
