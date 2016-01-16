aa0889d4ee2eaf23a41b41dde03dd38f3026bcd5
saeedalvand <saeedalvandkoohi@yahoo.com>
2016-01-16 16:59:51 +0330

    mostly designing profile page

--- app/src/main/java/ir/ac/ut/berim/ProfileActivity.java
+++ app/src/main/java/ir/ac/ut/berim/ProfileActivity.java
@@ -1,20 +1,34 @@
 package ir.ac.ut.berim;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Button logoutButton;
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_profile);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        logoutButton = (Button) findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProfileUtils.logoutUser(mContext);//todo do we need this?
+                Intent intent = new Intent(mContext, RegisterActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
 
     }
 
