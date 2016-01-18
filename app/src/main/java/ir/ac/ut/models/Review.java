be854140c01d390273e1c211cdf00d10cc579e37
masoud <eng.masood88@gmail.com>
2016-01-18 18:38:23 +0330

    some change

--- app/src/main/java/ir/ac/ut/models/Review.java
+++ app/src/main/java/ir/ac/ut/models/Review.java
@@ -3,10 +3,12 @@ package ir.ac.ut.models;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Serializable;
+
 /**
  * Created by saeed on 11/7/2015.
  */
-public class Review {
+public class Review implements Serializable {
 
     private User user;
 
