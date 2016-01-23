9c8baf8f15f75cb9185f1a6d9f7a996a74ec1fdc
saeedalvand <saeedalvandkoohi@yahoo.com>
2016-01-23 03:57:18 +0330

    last push

--- app/src/main/java/ir/ac/ut/adapter/RoomAdapter.java
+++ app/src/main/java/ir/ac/ut/adapter/RoomAdapter.java
@@ -1,6 +1,9 @@
 package ir.ac.ut.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,7 @@ import java.util.ArrayList;
 import ir.ac.ut.berim.ProfileUtils;
 import ir.ac.ut.berim.R;
 import ir.ac.ut.models.Message;
+import ir.ac.ut.utils.ImageLoader;
 
 /**
  * Created by saeed on 11/24/2015.
@@ -24,6 +28,8 @@ public class RoomAdapter extends BaseAdapter {
 
     private Context mContext;
 
+    private static final int FROM_ME = 0;
+    private static final int FROM_HER = 1;
     private ArrayList<Message> mMessages;
 
     public RoomAdapter(Context context, ArrayList<Message> data) {
@@ -59,31 +65,45 @@ public class RoomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        Message chatMessage = mMessages.get(position);
-        MessageViewHolder viewHolder;
+        int listViewItemType = getItemViewType(position);
+        final Message chatMessage = mMessages.get(position);
+        MessageViewHolder viewHolder = null;
         if (convertView == null) {
-            convertView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.item_chat_message, parent, false);
-            viewHolder = new MessageViewHolder(convertView);
-            convertView.setTag(viewHolder);
+            if (listViewItemType == FROM_ME) {
+                convertView = LayoutInflater.from(mContext)
+                        .inflate(R.layout.item_chat_message, parent, false);
+                viewHolder = new MessageViewHolder(convertView);
+                convertView.setTag(viewHolder);
+
+            } else {//IT'S A MESSEGE FROM OTHERS
+                convertView = LayoutInflater.from(mContext)
+                        .inflate(R.layout.item_chat_message_her, parent, false);
+                viewHolder = new MessageViewHolder(convertView);
+                viewHolder.sender.setText(chatMessage.getSender().getValidUserName());
+                convertView.setTag(viewHolder);
+                ImageLoader.getInstance()
+                        .display(chatMessage.getSender().getAvatar(), viewHolder.icon,
+                                R.drawable.default_avatar);
+            }
         } else {
             viewHolder = (MessageViewHolder) convertView.getTag();
         }
 
         viewHolder.name.setText(chatMessage.getText());
-        LinearLayout chatLayout = (LinearLayout) convertView
-                .findViewById(R.id.chat_background_linear_layout);
-        LinearLayout chatScrennLayout = (LinearLayout) convertView
-                .findViewById(R.id.chat_screen_linear_layout);
-
-        if (ProfileUtils.getUser(mContext).getId()
-                .equals(chatMessage.getSender().getId())) {
-            chatLayout.setBackgroundResource(R.drawable.bubble);
-            chatScrennLayout.setGravity(Gravity.RIGHT);
+        if (chatMessage.getFileAddress() != null || !chatMessage.getFileAddress().toLowerCase().equals("")) {
+            String[] fileName = chatMessage.getFileAddress().split("/");
+            Log.wtf("has file", chatMessage.getFileAddress());
+            viewHolder.inAppImage.setVisibility(View.VISIBLE);
+            ImageLoader.getInstance().display(chatMessage.getFileAddress(), viewHolder.inAppImage, R.drawable.ic_action_attach);
+            viewHolder.inAppImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent download = new Intent(Intent.ACTION_VIEW, Uri.parse(chatMessage.getFileAddress()));
+                    mContext.startActivity(download);
+                }
+            });
         } else {
-            chatLayout.setBackgroundResource(R.drawable.theirbubble);
-            chatScrennLayout.setGravity(Gravity.LEFT);
+            viewHolder.inAppImage.setVisibility(View.GONE);
         }
         return convertView;
     }
@@ -94,13 +114,16 @@ public class RoomAdapter extends BaseAdapter {
         final TextView name;
 
         final TextView description;
-
+        final TextView sender;
+        final ImageView inAppImage;
         final ImageView icon;
 
         MessageViewHolder(View view) {
             name = (TextView) view.findViewById(R.id.message_name_text_view);
             description = (TextView) view.findViewById(R.id.message_description_text_view);
-            icon = (ImageView) view.findViewById(R.id.list_icon_image);
+            sender = (TextView) view.findViewById(R.id.chat_sender);
+            icon = (ImageView) view.findViewById(R.id.chat_sender_avatar);
+            inAppImage = (ImageView) view.findViewById(R.id.in_chat_image);
         }
     }
 }
